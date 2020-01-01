f7eeeadb496c9bff2d823ef9f997a6b327343947
bin <zhou@163.com>
2020-01-01 21:05:01 +0800

    First Commit

---
+++ MyBean/WyFirstModule/src/com/MyCast/ShowUSer.java
@@ -0,0 +1,11 @@
+package com.MyCast;
+
+/**
+ * @author zhouYanBin
+ * @date 2020/1/1  21:02
+ */
+public class ShowUSer {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
