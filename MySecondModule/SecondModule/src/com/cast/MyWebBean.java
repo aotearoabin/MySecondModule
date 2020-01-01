355712db14d8b1aa6fe6b4138622e8a494cc087c
bin <zhou@163.com>
2020-01-01 21:08:56 +0800

    AAAAAAAAAAAAAAAAAAAAAA

---
+++ MySecondModule/SecondModule/src/com/cast/MyWebBean.java
@@ -0,0 +1,11 @@
+package com.cast;
+
+/**
+ * @author zhouYanBin
+ * @date 2020/1/1  21:07
+ */
+public class MyWebBean {
+    public static void main(String[] args) {
+        System.out.println("工程根路径");
+    }
+}
